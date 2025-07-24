614d70684e0a163876188ce36c70738b86beaebe
Nillan Sivarasa <nillan.sivarasa@gmail.com>
2025-07-24 12:01:46 +0200

    add test gui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,25 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
+	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) {
+		Scene scene = new Scene(createContent());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	private Region createContent() {
+		return new Label("Hello World");
 	}
 }
