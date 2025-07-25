6434f6a575144a500a42380c896c97854b7f4827
Nillan Sivarasa <nillan.sivarasa@gmail.com>
2025-07-25 16:43:27 +0200

    changed from textarea to textfield

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,9 @@ package org.example;
 
 import java.util.Objects;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -14,25 +15,20 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class Main extends Application {
 
-	/* Globally available */
-	private Stage stage;
-	private Scene curr_Scene;
-	private boolean check_if_clear_needed = true;
-
 	/* Scaling */
 	private final double WIDTH = 500;
 	private final double HEIGHT = 600;
-	private final double NUMBER_ROW_HEIGHT = HEIGHT / 5;
-	private final double NUMBER_ROW_WIDTH = WIDTH / 4;
+	private final double BUTTON_HEIGHT = HEIGHT / 5;
+	private final double BUTTON_WIDTH = WIDTH / 5;
 
 	/* Icon from: https://www.flaticon.com/free-icon/calculator_1011863 */
 	private final Image logo = new Image("https://shorturl.at/uParh");
 
-	/* Contains all number and operation rows */
+	/* Contains the display and number/operation buttons */
 	private final VBox calculator = new VBox();
 
-	/* TextArea functions as a display for input/output */
-	private final TextArea display_input_and_solution = new TextArea();
+	/* TextField functions as a display for input/output */
+	private final TextField display = new TextField();
 
 	/* HBoxes for the buttons */
 	private HBox input_and_solution;
@@ -62,11 +58,14 @@ public class Main extends Application {
 	private final Button b_comma  = new Button(".");
 	private final Button b_root  = new Button("√");
 	private final Button b_pow  = new Button("x²");
+	private final Button b_pow_x  = new Button("xˣ");
 	private final Button b_enter = new Button("=");
-	private final Button b_delete = new Button("⌫"); // TODO: delete sign
-	private final Button b_left_bracket = new Button("("); // TODO: Add this button
+	private final Button b_delete = new Button("⌫");
+	private final Button b_left_bracket = new Button("(");
 	private final Button b_right_bracket = new Button(")");
-	private final Button b_clear = new Button("C"); // TODO: Add this button
+	private final Button b_clear = new Button("C");
+	private final Button b_percent = new Button("%");
+	private final Button b_pi = new Button("π");
 
 	/* Main method to start the Application */
 	public static void main(String[] args) {
@@ -75,25 +74,24 @@ public class Main extends Application {
 
 	/* Starts the Application */
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage calculatorStage) {
 		init_calculator_ui();
 
 		/* Stage setup */
-		this.stage = primaryStage;
-		this.stage.setTitle("");
-		this.stage.setResizable(false);
-		this.stage.setWidth(WIDTH);
-		this.stage.setHeight(HEIGHT);
-		this.stage.getIcons().add(this.logo);
+		calculatorStage.setTitle("");
+		calculatorStage.setResizable(false);
+		calculatorStage.setWidth(WIDTH);
+		calculatorStage.setHeight(HEIGHT);
+		calculatorStage.getIcons().add(this.logo);
 
 		/* Scene setup */
-		this.curr_Scene = new Scene(displayCalculator());
-		this.curr_Scene.getStylesheets().add(Objects.requireNonNull(
+		Scene calculatorScene = new Scene(displayCalculator());
+		calculatorScene.getStylesheets().add(Objects.requireNonNull(
 			getClass().getResource("/style.css")).toExternalForm());
 
 
-		this.stage.setScene(this.curr_Scene);
-		this.stage.show();
+		calculatorStage.setScene(calculatorScene);
+		calculatorStage.show();
 	}
 
 	/* Returns the calculator */
@@ -110,7 +108,7 @@ public class Main extends Application {
 
 		init_operations_row();
 
-		init_textarea();
+		init_display();
 
 		init_calculator();
 	}
@@ -125,155 +123,181 @@ public class Main extends Application {
 		);
 	}
 
-	/* Init TextArea field */
-	private void init_textarea() {
-		this.display_input_and_solution.setText("0");
-		this.display_input_and_solution.setEditable(false);
-		this.display_input_and_solution.setFocusTraversable(false);
-		this.display_input_and_solution.setPrefWidth(WIDTH);
-		this.display_input_and_solution.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.display_input_and_solution.getStyleClass().add("my-textarea");
+	/* Init TextField */
+	private void init_display() {
+		this.display.setAlignment(Pos.CENTER);
+		this.display.setPromptText("0");
+		this.display.setEditable(false);
+		this.display.setFocusTraversable(false);
+		this.display.setPrefWidth(WIDTH);
+		this.display.setPrefHeight(BUTTON_HEIGHT);
+		this.display.getStyleClass().add("my-display");
 
-		this.input_and_solution = new HBox(this.display_input_and_solution);
+		this.input_and_solution = new HBox(this.display);
 		this.input_and_solution.setPrefWidth(WIDTH);
-		this.input_and_solution.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.input_and_solution.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Init operations row */
 	private void init_operations_row() {
-		this.b_plus.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_plus.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_plus.setPrefHeight(BUTTON_HEIGHT);
+		this.b_plus.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_plus);
 		button_pressed_action_for_operations_and_numbers(this.b_plus, "+");
 
-		this.b_minus.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_minus.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_minus.setPrefHeight(BUTTON_HEIGHT);
+		this.b_minus.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_minus);
 		button_pressed_action_for_operations_and_numbers(this.b_minus, "-");
 
-		this.b_multiplication.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_multiplication.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_multiplication.setPrefHeight(BUTTON_HEIGHT);
+		this.b_multiplication.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_multiplication);
 		button_pressed_action_for_operations_and_numbers(this.b_multiplication, "*");
 
-		this.b_division.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_division.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_division.setPrefHeight(BUTTON_HEIGHT);
+		this.b_division.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_division);
 		button_pressed_action_for_operations_and_numbers(this.b_division, "/");
 
+		this.b_delete.setPrefHeight(BUTTON_HEIGHT);
+		this.b_delete.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_delete);
+		button_pressed_action_for_operations_and_numbers(this.b_delete, "del");
+
 		this.operations_row = new HBox(this.b_plus,
-			this.b_minus, this.b_multiplication, this.b_division
+			this.b_minus, this.b_multiplication, this.b_division, this.b_delete
 		);
 		this.operations_row.setPrefWidth(WIDTH);
-		this.operations_row.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.operations_row.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Init first number row */
 	private void init_first_number_row() {
-		this.b1.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b1.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b1.setPrefHeight(BUTTON_HEIGHT);
+		this.b1.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b1);
 		button_pressed_action_for_operations_and_numbers(this.b1, "1");
 
-		this.b2.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b2.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b2.setPrefHeight(BUTTON_HEIGHT);
+		this.b2.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b2);
 		button_pressed_action_for_operations_and_numbers(this.b2, "2");
 
-		this.b3.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b3.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b3.setPrefHeight(BUTTON_HEIGHT);
+		this.b3.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b3);
 		button_pressed_action_for_operations_and_numbers(this.b3, "3");
 
-		this.b_root.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_root.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_root.setPrefHeight(BUTTON_HEIGHT);
+		this.b_root.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_root);
 		button_pressed_action_for_operations_and_numbers(this.b_root, "sqrt(");
 
-		this.first_number_row = new HBox(this.b1, this.b2, this.b3, this.b_root);
+		this.b_percent.setPrefHeight(BUTTON_HEIGHT);
+		this.b_percent.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_percent);
+		button_pressed_action_for_operations_and_numbers(this.b_percent, "%");
+
+		this.first_number_row = new HBox(this.b1, this.b2, this.b3, this.b_root, this.b_percent);
 		this.first_number_row.setPrefWidth(WIDTH);
-		this.first_number_row.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.first_number_row.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Init second number row */
 	private void init_second_number_row() {
-		this.b4.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b4.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b4.setPrefHeight(BUTTON_HEIGHT);
+		this.b4.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b4);
 		button_pressed_action_for_operations_and_numbers(this.b4, "4");
 
-		this.b5.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b5.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b5.setPrefHeight(BUTTON_HEIGHT);
+		this.b5.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b5);
 		button_pressed_action_for_operations_and_numbers(this.b5, "5");
 
-		this.b6.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b6.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b6.setPrefHeight(BUTTON_HEIGHT);
+		this.b6.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b6);
 		button_pressed_action_for_operations_and_numbers(this.b6, "6");
 
-		this.b_pow.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_pow.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_pow.setPrefHeight(BUTTON_HEIGHT);
+		this.b_pow.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_pow);
 		button_pressed_action_for_operations_and_numbers(this.b_pow, "^2");
 
-		this.second_number_row = new HBox(this.b4, this.b5, this.b6, this.b_pow);
+		this.b_pow_x.setPrefHeight(BUTTON_HEIGHT);
+		this.b_pow_x.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_pow_x);
+		button_pressed_action_for_operations_and_numbers(this.b_pow_x, "^");
+
+		this.second_number_row = new HBox(this.b4, this.b5, this.b6, this.b_pow, this.b_pow_x);
 		this.second_number_row.setPrefWidth(WIDTH);
-		this.second_number_row.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.second_number_row.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Init third number row */
 	private void init_third_number_row() {
-		this.b7.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b7.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b7.setPrefHeight(BUTTON_HEIGHT);
+		this.b7.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b7);
 		button_pressed_action_for_operations_and_numbers(this.b7, "7");
 
-		this.b8.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b8.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b8.setPrefHeight(BUTTON_HEIGHT);
+		this.b8.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b8);
 		button_pressed_action_for_operations_and_numbers(this.b8, "8");
 
-		this.b9.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b9.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b9.setPrefHeight(BUTTON_HEIGHT);
+		this.b9.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b9);
 		button_pressed_action_for_operations_and_numbers(this.b9, "9");
 
-		this.b_delete.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_delete.setPrefWidth(NUMBER_ROW_WIDTH);
-		style_button(this.b_delete);
-		button_pressed_action_for_operations_and_numbers(this.b_delete, "del");
+		this.b_right_bracket.setPrefHeight(BUTTON_HEIGHT);
+		this.b_right_bracket.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_right_bracket);
+		button_pressed_action_for_operations_and_numbers(this.b_right_bracket, ")");
 
-		this.third_number_row = new HBox(this.b7, this.b8, this.b9, this.b_delete);
+		this.b_left_bracket.setPrefHeight(BUTTON_HEIGHT);
+		this.b_left_bracket.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_left_bracket);
+		button_pressed_action_for_operations_and_numbers(this.b_left_bracket, "(");
+
+		this.third_number_row = new HBox(this.b7, this.b8, this.b9, this.b_left_bracket, this.b_right_bracket);
 		this.third_number_row.setPrefWidth(WIDTH);
-		this.third_number_row.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.third_number_row.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Init fourth number row */
 	private void init_fourth_number_row() {
-		this.b0.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b0.setPrefWidth(NUMBER_ROW_WIDTH);
-		style_button(this.b0);
-		button_pressed_action_for_operations_and_numbers(this.b0, "0");
+		this.b_clear.setPrefHeight(BUTTON_HEIGHT);
+		this.b_clear.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_clear);
+		button_pressed_action_for_operations_and_numbers(this.b_clear, "clear");
 
-		this.b_comma.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_comma.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b_comma.setPrefHeight(BUTTON_HEIGHT);
+		this.b_comma.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_comma);
 		button_pressed_action_for_operations_and_numbers(this.b_comma, ".");
 
-		this.b_enter.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_enter.setPrefWidth(NUMBER_ROW_WIDTH);
+		this.b0.setPrefHeight(BUTTON_HEIGHT);
+		this.b0.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b0);
+		button_pressed_action_for_operations_and_numbers(this.b0, "0");
+
+		this.b_pi.setPrefHeight(BUTTON_HEIGHT);
+		this.b_pi.setPrefWidth(BUTTON_WIDTH);
+		style_button(this.b_pi);
+		button_pressed_action_for_operations_and_numbers(this.b_pi, "π");
+
+		this.b_enter.setPrefHeight(BUTTON_HEIGHT);
+		this.b_enter.setPrefWidth(BUTTON_WIDTH);
 		style_button(this.b_enter);
 		button_pressed_action_for_operations_and_numbers(this.b_enter, "=");
 
-		this.b_right_bracket.setPrefHeight(NUMBER_ROW_HEIGHT);
-		this.b_right_bracket.setPrefWidth(NUMBER_ROW_WIDTH);
-		style_button(this.b_right_bracket);
-		button_pressed_action_for_operations_and_numbers(this.b_right_bracket, ")");
-
-		this.fourth_number_row = new HBox(this.b_comma, this.b0, this.b_right_bracket, this.b_enter);
+		this.fourth_number_row = new HBox(this.b_clear, this.b0, this.b_pi, this.b_comma, this.b_enter);
 		this.fourth_number_row.setPrefWidth(WIDTH);
-		this.fourth_number_row.setPrefHeight(NUMBER_ROW_HEIGHT);
+		this.fourth_number_row.setPrefHeight(BUTTON_HEIGHT);
 	}
 
 	/* Styling for buttons using css (resources/style.css) */
@@ -292,22 +316,19 @@ public class Main extends Application {
 			else if(b.equals(this.b_delete))
 				handle_delete_action();
 
+			/* User pressed clear button */
+			else if(b.equals(this.b_clear))
+				this.display.clear();
+
 			/* User pressed other button */
 			else
-				handle_button(action);
+				this.display.appendText(action);
 		});
 	}
 
 	/* Helper functions for button events */
-	private void handle_button(String action) {
-		if(this.check_if_clear_needed)
-			this.display_input_and_solution.clear();
-		this.check_if_clear_needed = false;
-		this.display_input_and_solution.appendText(action);
-	}
-
 	private void handle_delete_action() {
-		String input = this.display_input_and_solution.getText();
+		String input = this.display.getText();
 		if(input.isEmpty())
 			return;
 		char[] input_as_char = input.toCharArray();
@@ -321,18 +342,21 @@ public class Main extends Application {
 		*/
 		System.arraycopy(input_as_char, 0, sol, 0, input_as_char.length - 1);
 
-		this.display_input_and_solution.setText(String.valueOf(sol));
+		this.display.setText(String.valueOf(sol));
 	}
 
 	private void handle_enter_action() {
-		String input = this.display_input_and_solution.getText();
+		String input = this.display.getText();
 		try {
-			Expression exp = new ExpressionBuilder(input).build();
-			this.display_input_and_solution.clear();
-			this.display_input_and_solution.setText(String.valueOf(exp.evaluate()));
-			this.check_if_clear_needed = true;
+			Expression exp = new ExpressionBuilder(input)
+				.variable("π")
+				.build()
+				.setVariable("π", Math.PI);
+
+			this.display.clear();
+			this.display.setText(String.valueOf(exp.evaluate()));
 		} catch (Exception e) {
-			System.err.println("Error calculating the solution: " + e.getMessage());
+			this.display.setText("Syntax Error");
 		}
 	}
 }
